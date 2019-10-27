441bdf5a0d1a6de510339877966a6345a0ea4bd0
woochanleee <030219woo@naver.com>
2019-10-27 14:55:18 +0900

    Typing

---
+++ 정적 타이핑/3. 타입 추론(Type Inference)/example1.ts
@@ -0,0 +1,2 @@
+let foo = 123; // foo는 number 타입
+foo = 'hi';    // error: Type '"hi"' is not assignable to type 'number'.
---
+++ 정적 타이핑/3. 타입 추론(Type Inference)/example2.ts
@@ -0,0 +1,7 @@
+let foo; // let foo: any와 동치
+
+foo = 'Hello';
+console.log(typeof foo); // string
+
+foo = true;
+console.log(typeof foo); // boolean
